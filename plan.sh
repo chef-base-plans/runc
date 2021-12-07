aa909363c76e6954a8c1caf889342a615cbd6b63
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-12-07 19:35:49 +0530

    Version bumped from 1.0.0-rc93 to 1.0.3
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 pkg_name="runc"
 pkg_origin="core"
-pkg_version=1.0.0-rc93
+pkg_version=1.0.3
 pkg_description="CLI tool for spawning and running containers according to the OCI specification"
 pkg_upstream_url="https://www.opencontainers.org/"
 pkg_license=('Apache-2.0')
